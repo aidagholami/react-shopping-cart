346099eab6686d06fb4b479ec1e58d59220d8a5c
Aida Gholami <ayda.gholamy@gmail.com>
2020-10-19 20:53:30 +1100

    create orders

--- server.js
+++ server.js
@@ -43,5 +43,34 @@ app.delete('/api/products/:id', async (req, res) => {
   res.send(deletedProduct);
 })
 
+const Order = mongoose.model('order', new mongoose.Schema({
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  email: String,
+  name: String,
+  address: String,
+  total: Number,
+  cartItems: [{
+    _id: String,
+    title: String,
+    price: Number,
+    count: Number
+  }]
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }))
+
+app.post('/api/orders', async (req, res) => {
+  if (!req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems) {
+    return res.send({ message: 'Data is required.' });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('serve at http://localhost:5000'));
