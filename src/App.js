2bcfdbc3dba555e931a6d0e51b62618be4f3c879
Aida Gholami <ayda.gholamy@gmail.com>
2020-10-19 14:41:38 +1100

    add redux to filter component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import { Provider } from 'react-redux';
 import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
-import data from './data.json';
 import store from './store';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      products: data.products,
-      currentAction: {
-        size: '',
-        sort: ''
-      },
       cartItems: JSON.parse(localStorage.getItem('cartItems')) ? JSON.parse(localStorage.getItem('cartItems')) : []
     }
   }
@@ -46,42 +40,6 @@ class App extends Component {
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }
 
-  filter = (event) => {
-    const action = event.target.id;
-
-    if (action === 'size') {
-      const size = event.target.value;
-      this.setState(prevState => (
-        {
-          currentAction: {
-            size: size,
-            sort: prevState.currentAction.sort,
-          },
-          products: data.products.slice().filter(p => p.availableSizes.indexOf(size) >= 0).sort((a, b) => (
-            prevState.currentAction.sort === 'lowest' ? ((a.price > b.price)? 1 : -1):
-            prevState.currentAction.sort === 'highest' ? ((a.price < b.price)? 1 : -1):
-            ((a._id < b._id)? 1 : -1)
-          ))
-        }
-      ))
-    } else if (action === 'sort') {
-      const sort = event.target.value;
-      this.setState(prevState => (
-        {
-          currentAction: {
-            size: prevState.currentAction.size,
-            sort: sort
-          },
-          products: data.products.slice().filter(p => p.availableSizes.indexOf(prevState.currentAction.size) >= 0).sort((a, b) => (
-            sort === 'lowest' ? ((a.price > b.price)? 1 : -1):
-            sort === 'highest' ? ((a.price < b.price)? 1 : -1):
-            ((a._id < b._id)? 1 : -1)
-          ))
-        }
-      ))
-    }
-  }
-
   render() {
     return (
       <Provider store={store}>
@@ -92,13 +50,8 @@ class App extends Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter 
-                count={this.state.products.length} 
-                size={this.state.size} 
-                sort={this.state.sort} 
-                filterProducts={this.filter} 
-                sortProducts={this.filter}></Filter>
-                <Products products={this.state.products} addToCart={this.addToCart}></Products>
+                <Filter store={store} ></Filter>
+                <Products addToCart={this.addToCart}></Products>
               </div>
               <div className="sidebar">
                 <Cart cartItems = {this.state.cartItems} removeFromCart={this.removeFromCart} createOrder={this.createOrder}></Cart>
